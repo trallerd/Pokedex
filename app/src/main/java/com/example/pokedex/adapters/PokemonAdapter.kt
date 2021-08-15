308149173420729beeba58b299a8252171ae9632
trallerd <jessicacristec@gmail.com>
2021-08-15 17:03:45 -0300

    Toques finais

--- app/src/main/java/com/example/pokedex/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapters/PokemonAdapter.kt
@@ -16,17 +16,16 @@ import kotlinx.android.synthetic.main.pokemon_file.view.*
 import java.util.*
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-class PokemonAdapter(): RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+class PokemonAdapter: RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
     private val dao: PokemonDAO = PokemonDAO()
     private var pokemons = listOf<Result>()
 
     init {
-        dao.getALL(0,100) { apiResult ->
+        dao.getALL(898,0) { apiResult ->
             pokemons = apiResult.results
             notifyDataSetChanged()
         }
     }
-
     override fun getItemCount(): Int {
         return pokemons.size
     }
@@ -39,12 +38,15 @@ class PokemonAdapter(): RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
         )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val pokemon = pokemons[position]
         holder.fillView(pokemon)
+
     }
 
     inner class ViewHolder(item: View): RecyclerView.ViewHolder(item){
         fun fillView(pokemon: Result){
+
             Controller.fav = false
             itemView.pokheight.text = pokemon.name.uppercase(Locale.getDefault())
             dao.getByName(pokemon.name){pokemonAPI ->
